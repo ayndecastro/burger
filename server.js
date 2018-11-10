35b465ffce6e88fc08b773a16259456bf9abe981
ayn <vincentayndecastro@gmail.com>
2018-11-09 19:11:11 -0600

    last

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const express = require("express");
+// Pull in required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-let PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 3000;
 
-const app = express();
+var app = express();
 
+// Serve static content for the app from the 'public' directory
+app.use(express.static(process.cwd() + '/public'));
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Set Handlebars as the view engine
+var exphbs = require('express-handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-const routes = require("./controller/burger_controller");
+// Import routes and give the server access to them
+var routes = require('./controllers/burgers_controller.js');
 
-app.use(routes);
+app.use('/', routes);
 
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
-})
+app.listen(PORT);
