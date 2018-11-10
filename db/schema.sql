9c0e76f1cca8b1633dc0cea4426509176a821c5b
ayn <vincentayndecastro@gmail.com>
2018-11-10 02:07:09 -0600

    final

--- db/schema.sql
+++ db/schema.sql
@@ -11,6 +11,3 @@ CREATE TABLE burgers
   	PRIMARY KEY(id)
 );
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false),
-('Steak Burger', false),('Cheeseburger', false),
-('Veggie Burger', true),('Brisket Burger', true)
